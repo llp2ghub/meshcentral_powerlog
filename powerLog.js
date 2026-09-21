6fc90989ff174d9ac775c9ef728cce521c93f7bc
llp2ghub <lamoyne@scsolutions.com>
2026-09-20 18:17:39 -0700

    updated host with devicename

--- powerLog.js
+++ powerLog.js
@@ -143,7 +143,7 @@ module2.exports[PLUGIN_2_SHORT_NAME] = function (pluginHandler) {
 
       const payload = JSON.stringify({
         version: '1.1',
-        host: 'meshcentral',
+        host: `${extraInfo?.name || 'unknown'}_meshcentral`,
         short_message: `${extraInfo?.name || nodeid} powered ${stateSet ? 'on' : 'off'}`,
         timestamp: connectTime / 1000,
         level: 6,
